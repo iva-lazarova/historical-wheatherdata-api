aaa40c7908fb80f310645a152cc395d304c5be84
iva <ivanela.lazarova@gmail.com>
2023-11-02 14:16:33 +0800

    Add url endpoint for yearly data and all data per station

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from flask import Flask, render_template
 import pandas as pd
+from datetime import datetime
 
 # Create a website object
 app = Flask(__name__)
 
 stations = pd.read_csv("data_small/stations.txt", skiprows=17)
-stations = stations[["STAID","STANAME                                 "]]
+stations = stations[["STAID", "STANAME                                 "]]
 
 # Connect html pages with website object
 @app.route("/")
 def home():
     return render_template("home.html", data=stations.to_html())
+
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
@@ -21,8 +23,24 @@ def about(station, date):
             "date": date,
             "temperature": temperature}
 
-# Give a specific port name to be able to run multiple Flask apps
-# E.g. port 5001
-# Avoids clashes between app running at the same time
+
+@app.route("/api/v1/<station>")
+def all_data(station):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20, parse_dates=["    DATE"])
+    # Change structure of dictionary to more readable format
+    result = df.to_dict(orient="records")
+    return result
+
+
+@app.route("/api/v1/yearly/<station>/<year>")
+def yearly(station, year):
+    filename = "data_small/TG_STAID" + str(station).zfill(6) + ".txt"
+    df = pd.read_csv(filename, skiprows=20)
+    df["    DATE"] = df["    DATE"].astype(str)
+    result = df[df["    DATE"].str.startswith(str(year))].to_dict(orient="records")
+    return result
+
+
 if __name__ == "__main__":
     app.run(debug=True)
